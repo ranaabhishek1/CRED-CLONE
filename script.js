aa33113bed25ae8e8b9b99d3b8fb435f2722ebe8
ranaabhishek1 <abhishekforbusiness1@gmail.com>
2024-03-19 22:53:59 +0530

    add content

--- script.js
+++ script.js
@@ -21,3 +21,119 @@ tl.to("#imgone,#imgfive",{
     marginBottom:"-3vh",
 })
 
+// ++++++
+
+let tl2=gsap.timeline({
+    scrollTrigger:{
+        trigger:"#part-5",
+        start:"40% 50%",
+        end:"150% 50%",
+
+        pin:true,
+        scrub:2,
+    },
+})
+
+tl2.to("#scroll-1",{
+    bottom:"60vh",
+})
+
+tl2.to("#scroll-1",{
+    opacity:0,
+})
+
+tl2.to("#scroll-2",{
+    opacity:1,
+},'ani1')
+
+
+tl2.to(".phone-img",{
+    x:"-31%",
+},'ani1')
+
+
+tl2.to("#scroll-2",{
+    bottom:"60vh",
+})
+
+
+
+tl2.to("#scroll-2",{
+    opacity:0,
+})
+
+
+tl2.to("#scroll-3",{
+    opacity:1,
+},'ani2')
+
+
+tl2.to(".phone-img",{
+    x:"-62%",
+},'ani2')
+
+
+
+// +++++
+
+
+tl2.to("#scroll-2",{
+    bottom:"60vh",
+})
+
+
+
+tl2.to("#scroll-2",{
+    opacity:0,
+})
+
+
+tl2.to("#scroll-3",{
+    opacity:1,
+})
+
+
+tl2.to("#scroll-3",{
+    bottom:"60vh",
+})
+
+
+tl2.to("#scroll-3",{
+    opacity:0,
+})
+
+
+tl2.to("#scroll-4",{
+    opacity:1,
+},'ani3')
+
+
+
+tl2.to(".phone-img",{
+    x:"-92%",
+},'ani3')
+
+// ++++
+
+
+ 
+
+// ++++++++++++
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    
+  });
+
+
+
+// +++++++++++++++
